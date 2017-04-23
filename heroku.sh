a2c4c3f786fd8dae0d4f6b43114cdadea496b3c7
Drew <misc.dmacht777@gmail.com>
2017-04-23 13:38:31 -0500

    re-daemon
    I really should set this up to run locally

--- heroku.sh
+++ heroku.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-newrelic-admin run-program gunicorn -w 4 -b 0.0.0.0:$PORT -k gevent app:app
+newrelic-admin run-program gunicorn -w 4 -b 0.0.0.0:$PORT -k gevent app:app --daemon
 python3 bot.py
