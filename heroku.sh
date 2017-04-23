a718dc28fcee270fc9e599fe22586ceff1259d8a
Drew <misc.dmacht777@gmail.com>
2017-04-23 10:18:28 -0500

    rrgh

--- heroku.sh
+++ heroku.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-newrelic-admin run-program gunicorn -w 4 -b 0.0.0.0:$PORT -k gevent app:app --daemon
+newrelic-admin run-program gunicorn -w 4 -b 0.0.0.0:$PORT -k gevent app:app
 python3 bot.py
