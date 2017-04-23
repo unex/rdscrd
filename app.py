18abb48590df02b053b0c36f2f81b0748367c393
Drew <misc.dmacht777@gmail.com>
2017-04-23 00:34:43 -0500

    z

--- app.py
+++ app.py
@@ -35,10 +35,14 @@ REDDIT_OAUTH_BASE_URL = "https://oauth.reddit.com/api/v1"
 
 app = Flask(__name__)
 
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
-
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
+
+@app.before_first_request
+def setup_logging():
+    if not app.debug:
+        # In production mode, add log handler to sys.stderr.
+        app.logger.addHandler(logging.StreamHandler())
+        app.logger.setLevel(logging.INFO)
 
 # open connection before each request
 @app.before_request
