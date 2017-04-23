5ca544bb47232e64197173af23dda82ffd7859f1
unex <7575866+unex@users.noreply.github.com>
2017-04-22 19:36:56 -0500

    Bot test

---
+++ bot.py
@@ -0,0 +1,32 @@
+import os
+
+import discord
+import asyncio
+
+DISCORD_CLIENT_ID = os.environ.get("DISCORD_CLIENT_ID")
+DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as {0}, {1}'.format(client.user.name, client.user.id))
+
+    print('-----------------------------------------')
+
+while True:
+    try:
+        loop.create_task(monitor_db())
+        loop.run_until_complete(client.login(DISCORD_TOKEN))
+        loop.run_until_complete(client.connect())
+
+    except Exception as e:
+        print(str(e))
+        print('WAITING BEFORE TRYING AGAIN')
+
+        loop.run_until_complete(client.close())
+
+    finally:
+        loop.close()
+
+    time.sleep(10)
---
+++ heroku.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+web: gunicorn -w 4 -b 0.0.0.0:$PORT -k gevent app:app
+python3 bot.py
